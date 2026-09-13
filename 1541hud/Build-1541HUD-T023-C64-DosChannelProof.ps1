a4af70841394979133c41d103838020185e7b382
benkrall1973 <benkrall1973@gmail.com>
2026-09-13 00:07:52 -0500

    Tokenize C64 BASIC equals operator in T0.0.23

--- 1541hud/Build-1541HUD-T023-C64-DosChannelProof.ps1
+++ 1541hud/Build-1541HUD-T023-C64-DosChannelProof.ps1
@@ -47,11 +47,12 @@ $PRINTN = 0x98
 $LEN    = 0xC3
 $CHR    = 0xC7
 $PLUS   = 0xAA
+$EQUALS = 0xB2
 
 $lines = @(
     [pscustomobject]@{ N = 10; B = [byte[]]($OPEN,49,53,44,56,44,49,53) },
     [pscustomobject]@{ N = 20; B = [byte[]]($PRINTN,49,53,44,34,77,45,82,34,$PLUS,$CHR,40,48,41,$PLUS,$CHR,40,54,41,$PLUS,$CHR,40,49,41) },
-    [pscustomobject]@{ N = 25; B = [byte[]]($FOR,73,61,49,$TO,49,48,48,58,$NEXT,73) },
+    [pscustomobject]@{ N = 25; B = [byte[]]($FOR,73,$EQUALS,49,$TO,49,48,48,58,$NEXT,73) },
     [pscustomobject]@{ N = 30; B = [byte[]]($GET,35,49,53,44,65,36,58,$PRINT,32,$LEN,40,65,36,41) },
     [pscustomobject]@{ N = 40; B = [byte[]]($CLOSE,49,53) }
 )
